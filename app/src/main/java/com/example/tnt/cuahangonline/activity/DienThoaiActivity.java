ac75a769926773a7a4ecbcb2e31a8e4ca2240279
tanda <tandai96@gmail.com>
2018-04-09 22:30:18 +0700

    get du lieu cho Man hinh dien thoai, fix loi get san pham

--- app/src/main/java/com/example/tnt/cuahangonline/activity/DienThoaiActivity.java
+++ app/src/main/java/com/example/tnt/cuahangonline/activity/DienThoaiActivity.java
@@ -2,15 +2,130 @@ package com.example.tnt.cuahangonline.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.tnt.cuahangonline.R;
+import com.example.tnt.cuahangonline.adapter.DienthoaiAdapter;
+import com.example.tnt.cuahangonline.model.Sanpham;
+import com.example.tnt.cuahangonline.ultil.CheckConnection;
+import com.example.tnt.cuahangonline.ultil.Server;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DienThoaiActivity extends AppCompatActivity {
 
 
+    Toolbar toolbardt;
+    ListView lvdt;
+    DienthoaiAdapter dienthoaiAdapter;
+    ArrayList<Sanpham> mangdt;
+    int iddt = 0;
+    int page = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dien_thoai);
+        Anhxa();
+        if (CheckConnection.haveNetworkConnection(getApplicationContext())){
+            GetIdloaisp();
+            ActionToolbar();
+            GetData(page);
+        }else {
+            CheckConnection.ShowToast_Short(getApplicationContext(),"Vui long ket noi mang");
+            finish();
+        }
+
+    }
+
+    private void GetData(int Page) {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        String duongdan = Server.Duongdandienthoai+ String.valueOf(Page);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, duongdan, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                int id = 0;
+                String Tendt = "";
+                int Giadt = 0;
+                String Hinhanhdt = "";
+                String Mota = "";
+                int Idspdt = 0;
+                if (response != null){
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        for (int i = 0; i< jsonArray.length(); i ++){
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            id = jsonObject.getInt("id");
+                            Tendt = jsonObject.getString("tensp");
+                            Giadt = jsonObject.getInt("giasp");
+                            Hinhanhdt = jsonObject.getString("hinhanhsp");
+                            Mota = jsonObject.getString("motasp");
+                            Idspdt = jsonObject.getInt("idsanpham");
+                            mangdt.add(new Sanpham(id, Tendt, Giadt, Hinhanhdt, Mota, Idspdt));
+                            dienthoaiAdapter.notifyDataSetChanged();
+
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> param = new HashMap<String,String>();
+                param.put("idsanpham",String.valueOf(iddt));
+                return param;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+
+
+    private void ActionToolbar() {
+        setSupportActionBar(toolbardt);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbardt.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void GetIdloaisp() {
+        iddt = getIntent().getIntExtra("idloaisanpham", -1);
+        Log.d("giatriloaisanpham", iddt+"");
+        
+    }
+
+    private void Anhxa() {
+        toolbardt = findViewById(R.id.toolbardienthoai);
+        lvdt = findViewById(R.id.listviewdienthoai);
+        mangdt = new ArrayList<>();
+        dienthoaiAdapter = new DienthoaiAdapter(getApplicationContext(), mangdt);
+        lvdt.setAdapter(dienthoaiAdapter);
     }
 }
--- app/src/main/java/com/example/tnt/cuahangonline/ultil/Server.java
+++ app/src/main/java/com/example/tnt/cuahangonline/ultil/Server.java
@@ -4,5 +4,5 @@ public class Server {
     public static String localhost = "newsmachine.website";//địa chỉ ip của máy
     public static String DuongdanLoaisp = "https://" + localhost + "/getloaisanpham.php";
     public static  String Duongdansanphammoinhat = "https://" + localhost + "/getsanphammoinhat.php";
-
+    public static  String Duongdandienthoai = "https://" + localhost + "/getsanpham.php?page=";
 }
