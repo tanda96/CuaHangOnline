621e140bced04d8efff0b1df4847b9f303460b9f
duydoo <ngobaduynbd@gmail.com>
2018-04-14 17:47:23 +0700

    custom dl

---
+++ app/src/main/java/com/example/tnt/cuahangonline/adapter/GiohangAdapter.java
@@ -0,0 +1,75 @@
+package com.example.tnt.cuahangonline.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.tnt.cuahangonline.R;
+import com.example.tnt.cuahangonline.model.Giohang;
+import com.squareup.picasso.Picasso;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import static com.example.tnt.cuahangonline.R.id.buttonminus;
+
+public class GiohangAdapter extends BaseAdapter {
+    Context context;
+    ArrayList<Giohang> arraygiohang;
+    public GiohangAdapter(Context context,ArrayList<Giohang> arraygiohang){
+        this.context = context;
+        this.arraygiohang = arraygiohang;
+    }
+    @Override
+    public int getCount() {
+        return arraygiohang.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return arraygiohang.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+    public class ViewHolder{
+        public TextView txttengiohang,txtgiagiohang;
+        public ImageView imggiohang;
+        public Button btnminus,btnvalues,btnplus;
+    }
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder = null;
+        if (view == null){
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.dong_goihang,null);
+            viewHolder.txttengiohang =(TextView) view.findViewById(R.id.textviewtengiohang);
+            viewHolder.txtgiagiohang = (TextView) view.findViewById(R.id.textviewgiagiohang);
+            viewHolder.imggiohang = (ImageView) view.findViewById(R.id.imageviewgiohang);
+            viewHolder.btnminus = (Button) view.findViewById(R.id.buttonminus);
+            viewHolder.btnvalues = (Button)view.findViewById(R.id.buttonvalues);
+            viewHolder.btnplus =(Button) view.findViewById(R.id.buttonplus);
+            view.setTag(viewHolder);
+    } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        Giohang giohang= (Giohang) getItem(i);
+        viewHolder.txttengiohang.setText(giohang.getTensp());
+        DecimalFormat decimalFormat= new DecimalFormat("###,###,###");
+        viewHolder.txtgiagiohang.setText(decimalFormat.format(giohang.getGiasp()) + "Đ");
+        Picasso.with(context).load(giohang.getHinhsp())
+                .placeholder(R.drawable.noimage)
+                .error(R.drawable.error)
+                .into(viewHolder.imggiohang);
+        viewHolder.btnvalues.setText(giohang.getSoluongsp() + "");
+        return view;
+    }
+}
