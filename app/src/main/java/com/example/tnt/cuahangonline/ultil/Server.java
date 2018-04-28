386bf6a970aad814203bbc175d4e6eb4e776c603
Phong Do Khac <phong@conceptual.studio>
2018-04-28 19:55:58 +0700

    change link to JSON child folder

--- app/src/main/java/com/example/tnt/cuahangonline/ultil/Server.java
+++ app/src/main/java/com/example/tnt/cuahangonline/ultil/Server.java
@@ -1,7 +1,7 @@
 package com.example.tnt.cuahangonline.ultil;
 
 public class Server {
-    public static String localhost = "newsmachine.website";//địa chỉ ip của máy
+    public static String localhost = "newsmachine.website/JSON";//địa chỉ ip của máy
     public static String DuongdanLoaisp = "https://" + localhost + "/getloaisanpham.php";
     public static  String Duongdansanphammoinhat = "https://" + localhost + "/getsanphammoinhat.php";
     public static  String Duongdandienthoai = "https://" + localhost + "/getsanpham.php?page=";
