ff23b960c7fb92b7578455c66a1ba738f1516e03
HoangNhanTNT <38106385+HoangNhanTNT@users.noreply.github.com>
2018-05-04 02:10:52 +0700

    Do Khac Phong
    Add dia chi va map cho map activity
    
    Co-Authored-By: phonggcs <phonggcs@users.noreply.github.com>

--- app/src/main/java/com/example/tnt/cuahangonline/activity/MapsActivity.java
+++ app/src/main/java/com/example/tnt/cuahangonline/activity/MapsActivity.java
@@ -1,30 +1,53 @@
 package com.example.tnt.cuahangonline.activity;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 import com.example.tnt.cuahangonline.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Toolbar toolbarmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        toolbarmap = findViewById(R.id.toolbarmap);
+        ActiontoolBar();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    private void ActiontoolBar() {
+        setSupportActionBar(toolbarmap);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbarmap.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -40,8 +63,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng location = new LatLng(10.8482599, 106.7841353);
-        mMap.addMarker(new MarkerOptions().position(location).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        LatLng location = new LatLng(10.7712952,106.6889006);
+        mMap.addMarker(new MarkerOptions().position(location).title("Shop Online Phleek").snippet("97 Man Thiện Quận 9 Phường Hiệp Phú, Quận 9 , TPHCM").icon(BitmapDescriptorFactory.defaultMarker()));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        CameraPosition cameraposition = new CameraPosition.Builder().target(location).zoom(40).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraposition));
     }
 }
